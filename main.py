fad9e17bddbd674bb6bfbd96e3af1d3812448817
DBTDerpbox <65347035+DBTDerpbox@users.noreply.github.com>
2023-01-20 12:45:07 -0600

    Poor attempt to fix /accept not working well

--- main.py
+++ main.py
@@ -70,12 +70,12 @@ async def accept(ctx: discord.Interaction, user: discord.Member):
     if discord.utils.get(user.guild.roles, name=role) in user.roles:
         await ctx.response.send_message("User <@"+str(user.id)+"> has already been verified!",ephemeral=True)
     else:
-        #Set user roles
-        await user.add_roles(discord.utils.get(user.guild.roles, name=role))
-        await user.remove_roles(discord.utils.get(user.guild.roles, name=oldRole))
         #Send message
         await user.send("You have been verified in the [Legacy Edition Minigames] server! You now have access to channels, welcome to the community!")
         await ctx.response.send_message(content="Verified <@"+str(user.id)+">!",ephemeral=True)
+        #Set user roles
+        await user.add_roles(discord.utils.get(user.guild.roles, name=role))
+        await user.remove_roles(discord.utils.get(user.guild.roles, name=oldRole))
         channel = discord.utils.get(user.guild.channels, name="verification-list")
         await channel.send(f"User {user.mention} has been verified by "+str(ctx.user)+"! <a:xof:1038932012544827402>")
 
